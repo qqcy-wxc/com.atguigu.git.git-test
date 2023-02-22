6ae4c7ada5c05944d283f27660104780ab123413
Wh <wh@qq.com>
2023-02-22 22:06:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author: WH
+ * @create: 2023-02-22 21:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
