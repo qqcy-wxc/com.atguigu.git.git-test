0b636d57fd738d2e9928b23244e0c1ceaa5636ce
Wh <wh@qq.com>
2023-02-22 22:20:20 +0800

    hot-fix commit-1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,8 +6,9 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git!123");
         System.out.println("hello git!2");
         System.out.println("hello git!3");
+        System.out.println("hello git!4");
     }
 }
