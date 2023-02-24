9a14a79e7509f4ac7f7c4d85490614f710cc6b43
WH <12159553+qqcy-wxc@user.noreply.gitee.com>
2023-02-24 13:00:50 +0000

    pull-commit2
    Signed-off-by: WH <12159553+qqcy-wxc@user.noreply.gitee.com>

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hot-fix!1");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull1111 test");
     }
 }
